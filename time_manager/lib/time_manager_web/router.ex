3e1dedaa860e507e6269f226a4782b73db4d4259
alin <alingavriliuc@hotmail.com>
2022-10-25 10:29:00 +0200

    ✨ Updated routes

--- time_manager/lib/time_manager_web/router.ex
+++ time_manager/lib/time_manager_web/router.ex
@@ -20,12 +20,38 @@ defmodule TimeManagerWeb.Router do
     get "/", PageController, :index
   end
 
+  ############################################################################################
+  # * index - renders a list of all items of the given resource type
+  # * show - renders an individual item by ID
+  # * new - renders a form for creating a new item
+  # * create - receives parameters for one new item and saves it in a data store
+  # * edit - retrieves an individual item by ID and displays it in a form for editing
+  # * update - receives parameters for one edited item and saves the item to a data store
+  # * delete - receives an ID for an item to be deleted and deletes it from a data store
+  ############################################################################################
+
   # Other scopes may use custom stacks.
   scope "/api", TimeManagerWeb do
     pipe_through :api
-    resources "/users", UserController
-    resources "/workingtimes", WorkingtimeController
-    resources "/clocks", ClockController
+
+    # users routes
+    get "/users", UserController, :index
+    get "/users/:userID", UserController, :show
+    post "/users", UserController, :create
+    put "/users/:userID", UserController, :update
+    delete "/users/:userID", UserController, :delete
+
+    # workingtimes routes
+    get "/workingtimes/:userID", WorkingtimeController, :index
+    get "/workingtimes/:userID/:id", WorkingtimeController, :show
+    post "/workingtimes/:userID", WorkingtimeController, :create
+    put "/workingtimes/:id", WorkingtimeController, :update
+    delete "/workingtimes/:id", WorkingtimeController, :delete
+
+    # clocks routes
+    get "/clocks/:userID", ClockController, :show
+    post "/clocks/:userID", ClockController, :create
+
   end
 
   # Enables LiveDashboard only for development
@@ -35,25 +61,25 @@ defmodule TimeManagerWeb.Router do
   # If your application does not have an admins-only section yet,
   # you can use Plug.BasicAuth to set up some basic authentication
   # as long as you are also using SSL (which you should anyway).
-  if Mix.env() in [:dev, :test] do
-    import Phoenix.LiveDashboard.Router
+  # if Mix.env() in [:dev, :test] do
+  #   import Phoenix.LiveDashboard.Router
 
-    scope "/" do
-      pipe_through :browser
+  #   scope "/" do
+  #     pipe_through :browser
 
-      live_dashboard "/dashboard", metrics: TimeManagerWeb.Telemetry
-    end
-  end
+  #     live_dashboard "/dashboard", metrics: TimeManagerWeb.Telemetry
+  #   end
+  # end
 
   # Enables the Swoosh mailbox preview in development.
   #
   # Note that preview only shows emails that were sent by the same
   # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
+  # if Mix.env() == :dev do
+  #   scope "/dev" do
+  #     pipe_through :browser
 
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
-  end
+  #     forward "/mailbox", Plug.Swoosh.MailboxPreview
+  #   end
+  # end
 end
