a13a275948fe2282f697da4f2fa054961e16fca2
alin <alingavriliuc@hotmail.com>
2022-10-25 09:57:34 +0200

    ✨  Added controller files

---
+++ time_manager/lib/time_manager_web/controllers/clock_controller.ex
@@ -0,0 +1,39 @@
+defmodule TimeManagerWeb.ClockController do
+
+  @moduledoc """
+    Clock controller
+  """
+
+  @doc """
+    - index - renders a list of all items of the given resource type
+    - show - renders an individual item by ID
+    - new - renders a form for creating a new item
+    - create - receives parameters for one new item and saves it in a data store
+    - edit - retrieves an individual item by ID and displays it in a form for editing
+    - update - receives parameters for one edited item and saves the item to a data store
+    - delete - receives an ID for an item to be deleted and deletes it from a data store
+  """
+
+  use TimeManagerWeb, :controller
+
+  def index() do
+  end
+
+  def show() do
+  end
+
+  def new() do
+  end
+
+  def create() do
+  end
+
+  def edit() do
+  end
+
+  def update() do
+  end
+
+  def delete() do
+  end
+end
