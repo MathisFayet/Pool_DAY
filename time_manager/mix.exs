dab69e376cb8c6de0d65ae4b4be0c892480155a8
dotruchon <dorian.truchon@epitech.eu>
2022-10-28 09:48:40 +0200

    :truck: Renamed project to be compliant with other parts.

--- time_manager/mix.exs
+++ time_manager/mix.exs
@@ -3,7 +3,7 @@ defmodule TimeManager.MixProject do
 
   def project do
     [
-      app: :time_manager,
+      app: :time-manager-elixir-app,
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -52,7 +52,8 @@ defmodule TimeManager.MixProject do
       {:faker, "~> 0.17"},
       {:plug, "~> 1.0"},
       {:corsica, "~> 1.0"},
-      {:timex, "~> 3.0"}
+      {:timex, "~> 3.0"},
+      {:useful, "~> 0.4.0"}
     ]
   end
 
